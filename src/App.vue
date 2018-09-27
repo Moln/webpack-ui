c780a21a4e3483f6ed1ac43c5c2d244c5a4bcb3b
Moln <moln.xie@gmail.com>
2018-09-27 17:40:13 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <json-editor :schema="schema" v-model="value" />
+    </div>
+</template>
+
+<script>
+    import JsonEditor from './components/JsonEditor'
+    import schema from './webpack-config-schema'
+
+    export default {
+        name: 'app',
+        components: {
+            JsonEditor
+        },
+        data() {
+            return {
+                schema: schema,
+                value: {}
+            }
+        },
+        mounted() {
+
+        },
+        watch: {
+            // value() {
+            // }
+        }
+    }
+</script>
---
+++ src/components/JsonEditor.vue
@@ -0,0 +1,32 @@
+<template>
+  <div ref="jsoneditor">
+    sdf
+  </div>
+</template>
+
+<script>
+import '@json-editor/json-editor'
+export default {
+  name: 'JsonEditor',
+  props: {
+    schema: Object,
+    value: Object
+  },
+    data() {
+      return {
+          editor: null
+      }
+    },
+    mounted() {
+        console.log(this);
+        const editor = new window.JSONEditor(this.$refs['jsoneditor'], {
+            schema: this.schema,
+            required_by_default: true
+        });
+        editor.setValue(this.value);
+        editor.on('change', () => this.$emit('input', editor.getValue()));
+
+        this.editor = editor
+    }
+}
+</script>
